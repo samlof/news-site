77efee582e26836fdfdc97dc507801927c9e23f7
Samuli Lof <samuli.lof@gmail.com>
2021-12-22 18:19:39 +0700

    Add spinners

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatButtonModule } from '@angular/material/button';
 import { NewsCardComponent } from './news-card/news-card.component';
 import { HomePageComponent } from './home-page/home-page.component';
@@ -29,6 +30,7 @@ import { ArticlePageComponent } from './article-page/article-page.component';
     MatListModule,
     MatIconModule,
     MatButtonModule,
+    MatProgressSpinnerModule,
   ],
   providers: [
     {
