a499e340ca5746d37bca8a01ae4e06195e01e8bf
Samuli Lof <samuli.lof@gmail.com>
2021-12-22 20:38:20 +0700

    Tests passing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,8 +20,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'oivan-news app is running!'
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('News');
   });
 });
